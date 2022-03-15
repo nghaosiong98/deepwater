96687aa74058a64ba0e65b9086f4650699f22af0
nghaosiong98 <nghaosiong98@gmail.com>
2022-03-15 20:58:59 +0800

    feat: adjust navbar color

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,13 +2,15 @@ import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar: FC = () => (
-  <div className="w-screen absolute bg-gray-500 flex items-center h-16 justify-end top-0">
-    <NavLink to="/" className="text-gray-200 hover:text-gray-300 px-10 flex-grow">
-      DeepWater
-    </NavLink>
-    <NavLink to="/about" className="text-gray-200 hover:text-gray-300 px-10">
-      About Us
-    </NavLink>
+  <div className="w-screen absolute bg-mantis flex items-center h-16 justify-end top-0">
+    <div className="container mx-auto flex justify-between items-center">
+      <NavLink to="/" className="text-gray-200 hover:text-gray-300 px-10 flex-grow font-bold">
+        DeepWater
+      </NavLink>
+      <NavLink to="/about" className="text-gray-200 hover:text-gray-300 hover:bg-avocado bg-sapGreen px-6 py-2 rounded-md font-bold drop-shadow-md">
+        Our Works
+      </NavLink>
+    </div>
   </div>
 );
 
